e8ba2e36df05417f9f756640cf33d3022f093e6a
Marshley <xmash16x@gmail.com>
2021-04-21 00:42:30 +0800

    Initial commit

---
+++ Data/Grading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GradingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradingSystem.Data
+{
+    public class Grading
+    {
+    }
+
+    public class GradingContext : DbContext
+    {
+        public GradingContext (DbContextOptions<GradingContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GradingSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Middlename { get; set; }
+        public string Course { get; set; }
+
+    }
+}
